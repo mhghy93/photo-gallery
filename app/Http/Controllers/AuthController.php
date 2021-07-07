16ea5a3e140d43f913a43da8cf838e5aeaa9beec
mustaque <hussainmustaque98407@gmail.com>
2021-07-07 16:44:57 +0530

    register user api created

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = $request->validate([
+            'name' => 'required',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $validator['name'],
+            'email' => $validator['email'],
+            'password' => Hash::make($validator['password']),
+        ]);
+
+        $token = $user->createToken('token')->plainTextToken;
+
+        $response = [
+            'data' => new UserResource($user),
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+}
